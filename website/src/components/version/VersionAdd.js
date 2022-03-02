f828ea004edfe187d4cf38d9f9d8ced7e484134e
VelocityLight <yjchit@126.com>
2022-03-02 11:09:18 +0800

    add versionadd dialog

--- website/src/components/version/VersionAdd.js
+++ website/src/components/version/VersionAdd.js
@@ -1,11 +1,35 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export const VersionAdd = () => {
-    const handleApplyChanges = React.useCallback(() => {
-    }, []);
+    const [open, setOpen] = React.useState(false);
+    const [scroll, setScroll] = React.useState('paper');
+    const [maxWidth, setMaxWidth] = React.useState('lg');
+    const descriptionElementRef = React.useRef(null);
+
+    const handleClickOpen = (scrollType) => () => {
+        setOpen(true);
+        setScroll(scrollType);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    React.useEffect(() => {
+        if (open) {
+        const { current: descriptionElement } = descriptionElementRef;
+        if (descriptionElement !== null) {
+            descriptionElement.focus();
+        }
+        }
+    }, [open]);
+
 
     return (
-        <Button variant="contained" onClick={handleApplyChanges}>Add</Button>
+        <Button variant="contained" onClick={handleClickOpen('paper')}>Create</Button>
     );
 }

7cfd023d2b812046914b3b63137f69b2a0a2dd3f
VelocityLight <yjchit@126.com>
2022-03-10 14:14:00 +0800

    add event switch case

--- internal/controller/github_webhook.go
+++ internal/controller/github_webhook.go
@@ -22,9 +22,13 @@ func GithubWebhookHandler(c *gin.Context) {
 
 	// handle event
 	switch event := event.(type) {
-	case *github.CreateEvent:
-		// handle event example
-	default:
-		c.JSON(200, gin.H{"status": "ok", "data": event})
+		case *github.IssuesEvent, *github.IssueCommentEvent:
+			// handle event
+			c.JSON(200, gin.H{"status": "ok", "type": "issue_type", "data": event})
+		case *github.PullRequestEvent:
+			// handle event
+			c.JSON(200, gin.H{"status": "ok", "type": "pr_type", "data": event})
+		default:
+			c.JSON(200, gin.H{"status": "ok", "type": "others"})
 	}
 }

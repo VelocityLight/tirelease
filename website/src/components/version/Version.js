adbdca1ccd42f71be609933b227eaf5d275bbfce
ichn-hu <zfhu16@gmail.com>
2022-03-18 14:56:49 +0800

    add

--- website/src/components/version/Version.js
+++ website/src/components/version/Version.js
@@ -6,6 +6,7 @@ import { VersionSearch } from "./VersionSearch";
 import { VersionAdd } from "./VersionAdd";
 import { useQuery } from "react-query";
 import { url } from "../../utils";
+import { Button } from "@mui/material";
 
 export default function Version() {
   const { isLoading, error, data } = useQuery("versions", () => {
@@ -19,21 +20,33 @@ export default function Version() {
     console.log("data", data);
   }
 
+  const [createVersion, setCreateVersion] = useState(false);
+
   return (
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       {data && (
         <Stack spacing={1}>
-          <Stack direction={"row"} justifyContent={"flex-start"} spacing={2}>
-            <VersionSearch />
-            <VersionAdd />
+          <Stack direction={"row"} justifyContent={"flex-end"} spacing={2}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setCreateVersion(true);
+              }}
+            >
+              Create version
+            </Button>
           </Stack>
-          <Stack direction={"row"} justifyContent={"flex-start"} spacing={2}>
-            <VersionTable data={data.data} />
-          </Stack>
+          <VersionTable data={data.data} />
         </Stack>
       )}
+      <VersionAdd
+        open={createVersion}
+        onClose={() => {
+          setCreateVersion(false);
+        }}
+      />
     </>
   );
 }
--- website/src/components/version/VersionAdd.js
+++ website/src/components/version/VersionAdd.js
@@ -1,35 +1,82 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
+import { FormHelperText, Input, Stack } from "@mui/material";
+import { useMutation, useQueryClient } from "react-query";
+import { url } from "../../utils";
+import axios from "axios";
 
-export const VersionAdd = () => {
-    const [open, setOpen] = React.useState(false);
-    const [scroll, setScroll] = React.useState('paper');
-    const [maxWidth, setMaxWidth] = React.useState('lg');
-    const descriptionElementRef = React.useRef(null);
-
-    const handleClickOpen = (scrollType) => () => {
-        setOpen(true);
-        setScroll(scrollType);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-    React.useEffect(() => {
-        if (open) {
-        const { current: descriptionElement } = descriptionElementRef;
-        if (descriptionElement !== null) {
-            descriptionElement.focus();
-        }
-        }
-    }, [open]);
-
-
-    return (
-        <Button variant="contained" onClick={handleClickOpen('paper')}>Create</Button>
-    );
-}
+export const VersionAdd = ({ open, onClose }) => {
+  const queryClient = useQueryClient();
+  const [version, setVersion] = React.useState("");
+  const create = useMutation(
+    (data) => {
+      return axios.post(url("version"), data);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("versions");
+        onClose();
+      },
+      onError: (e) => {
+        console.log("error", e);
+      },
+    }
+  );
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+      <DialogTitle>Create Version</DialogTitle>
+      <DialogContent sx={{ display: "flex", flexDirection: "column" }}>
+        <FormControl>
+          <InputLabel htmlFor="my-input">Email address</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
+        <DialogContentText margin={2}>Version</DialogContentText>
+        <Stack direction="row" spacing={2}>
+          <FormControl>
+            <InputLabel id="create-version">Version</InputLabel>
+            <Select
+              labelId="create-version"
+              id="create-version-select"
+              value={version}
+              label="Version"
+              onChange={(e) => {
+                setVersion(e.target.value);
+              }}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <TextField label="name"></TextField>
+          </FormControl>
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Close</Button>
+        <Button
+          onClick={() => {
+            create.mutate();
+          }}
+          variant="contained"
+        >
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};

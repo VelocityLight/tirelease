7ae8dbfc0ddc5d771ffe518d455fd2191fc7da49
VelocityLight <yjchit@126.com>
2022-04-08 11:33:14 +0800

    refactor: test case ignore

--- internal/service/webhook_issue_test.go
+++ internal/service/webhook_issue_test.go
@@ -6,7 +6,7 @@ import (
 	"tirelease/commons/database"
 	"tirelease/commons/git"
 	"tirelease/internal/entity"
-	"tirelease/internal/repository"
+	// "tirelease/internal/repository"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -48,52 +48,52 @@ func TestWebhookRefreshIssueV4(t *testing.T) {
 	assert.Equal(t, true, err == nil)
 }
 
-func TestCronRefreshIssuesV42(t *testing.T) {
-	// init
-	git.Connect(git.TestToken)
-	git.ConnectV4(git.TestToken)
-	database.Connect(generateConfig())
-	repos, err := repository.SelectRepo(&entity.RepoOption{})
-	if err != nil {
-		return
-	}
-	releaseVersions, err := repository.SelectReleaseVersion(&entity.ReleaseVersionOption{})
-	if err != nil {
-		return
-	}
+// func TestCronRefreshIssuesV42(t *testing.T) {
+// 	// init
+// 	git.Connect(git.TestToken)
+// 	git.ConnectV4(git.TestToken)
+// 	database.Connect(generateConfig())
+// 	repos, err := repository.SelectRepo(&entity.RepoOption{})
+// 	if err != nil {
+// 		return
+// 	}
+// 	releaseVersions, err := repository.SelectReleaseVersion(&entity.ReleaseVersionOption{})
+// 	if err != nil {
+// 		return
+// 	}
 
-	params := &RefreshIssueParams{
-		Repos: repos,
-		// BeforeHours:     -8760,
-		BeforeHours:     -720,
-		Batch:           10,
-		Total:           20,
-		IsHistory:       true,
-		ReleaseVersions: releaseVersions,
-		Order:           "DESC",
-	}
+// 	params := &RefreshIssueParams{
+// 		Repos: repos,
+// 		// BeforeHours:     -8760,
+// 		BeforeHours:     -720,
+// 		Batch:           10,
+// 		Total:           20,
+// 		IsHistory:       true,
+// 		ReleaseVersions: releaseVersions,
+// 		Order:           "DESC",
+// 	}
 
-	// detail
-	err = CronRefreshIssuesV4(params)
-	assert.Equal(t, true, err == nil)
-}
+// 	// detail
+// 	err = CronRefreshIssuesV4(params)
+// 	assert.Equal(t, true, err == nil)
+// }
 
 // tidb： 2021-07-02 02:49:13  / 2021-10-28 12:46:50 / 2021-11-17 06:59:24 / 2022-01-18 04:17:53
 // tiflow: 2022-03-09
 
-func TestRefreshIssueField(t *testing.T) {
-	// init
-	git.Connect(git.TestToken)
-	git.ConnectV4(git.TestToken)
-	database.Connect(generateConfig())
+// func TestRefreshIssueField(t *testing.T) {
+// 	// init
+// 	git.Connect(git.TestToken)
+// 	git.ConnectV4(git.TestToken)
+// 	database.Connect(generateConfig())
 
-	// detail
-	option := &entity.IssueOption{
-		ListOption: entity.ListOption{
-			OrderBy: "id",
-			Order:   "ASC",
-		},
-	}
-	err := RefreshIssueField(option)
-	assert.Equal(t, true, err == nil)
-}
+// 	// detail
+// 	option := &entity.IssueOption{
+// 		ListOption: entity.ListOption{
+// 			OrderBy: "id",
+// 			Order:   "ASC",
+// 		},
+// 	}
+// 	err := RefreshIssueField(option)
+// 	assert.Equal(t, true, err == nil)
+// }
